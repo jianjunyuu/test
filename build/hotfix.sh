51c546cd4abbd2e4f3a3b81929fb076469093bd6
yujianjun <mryujianjun.cn@gmail.com>
2020-05-13 17:08:20 +0800

    完成hotfix的脚本

--- build/hotfix.sh
+++ build/hotfix.sh
@@ -4,6 +4,11 @@ set -e
 
 # 获取执行脚本的第一个参数，并创建hotfix分支名
 VERSION=$1
+if test -z "$VERSION"; then
+  echo 'The parameter `version` is required.' >&2;
+  exit 128;
+fi
+
 BRANCH_NAME="hotfix-$VERSION"
 # 生产环境对应的分支
 BRANCH_PRODUCTION=master
@@ -11,7 +16,8 @@ BRANCH_PRODUCTION=master
 # 切换到生产分支
 git checkout ${BRANCH_PRODUCTION}
 # 创建指定上线版本的分支
-git branch ${BRANCH_NAME}
+git checkout -b ${BRANCH_NAME}
 
 # 如果成功，则推送到远程去
 git push origin ${BRANCH_NAME}
+exit 0;
--- build/release-hotfix.sh
+++ build/release-hotfix.sh
@@ -2,3 +2,25 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+# 获取执行脚本的第一个参数，并创建hotfix分支名
+VERSION=$1
+if test -z "$VERSION"; then
+  echo 'The parameter `version` is required.' >&2;
+  exit 128;
+fi
+
+BRANCH_NAME="hotfix-$VERSION"
+
+# release分支
+BRANCH_RELEASE=release
+# 开发环境分支
+BRANCH_DEV=dev
+
+git checkout ${BRANCH_NAME}
+
+# 直接创建release分支，如果有报错，则立即停止
+git checkout -b ${BRANCH_RELEASE}
+
+# 删除hotfix本地分支，远程分支可不删
+git branch -d ${BRANCH_NAME}
+exit 0;
