3b138c18f5e0e8540c2b3be8ae76364e7428f865
yujianjun <mryujianjun.cn@gmail.com>
2020-05-13 16:59:09 +0800

    完成hotfix的脚本

--- build/hotfix.sh
+++ build/hotfix.sh
@@ -4,14 +4,14 @@ set -e
 
 # 获取执行脚本的第一个参数，并创建hotfix分支名
 VERSION=$1
-BRANCH_NAME="hotfix$VERSION"
+BRANCH_NAME="hotfix-$VERSION"
 # 生产环境对应的分支
 BRANCH_PRODUCTION=master
 
 # 切换到生产分支
 git checkout ${BRANCH_PRODUCTION}
 # 创建指定上线版本的分支
-git checkout -b ${BRANCH_NAME}
+git branch ${BRANCH_NAME}
 
 # 如果成功，则推送到远程去
 git push origin ${BRANCH_NAME}
