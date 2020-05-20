1f21e54172dcb35ea743e4d39bd59d69e819e184
yujianjun <mryujianjun.cn@gmail.com>
2020-05-20 14:45:04 +0800

    新增检测dev是否跟本地分支同步

--- build/release-dev.sh
+++ build/release-dev.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 # 确保脚本抛出遇到的错误
 set -e
-set -x
 
 # 用户输入的分支名
 BRANCH_NAME=$1
@@ -10,11 +9,10 @@ BRANCH_RELEASE=${BRANCH_NAME:=release}
 # 开发环境分支
 BRANCH_DEV=dev
 
-git checkout ${BRANCH_DEV}
-
+#git checkout ${BRANCH_DEV}
 
 # 需要检测一下是否需要进行一下操作：检测远程的head是否同步于本地head
-if test "0" == "$(git rev-list --count ...HEAD)"; then
+if test -n "$(git diff ${BRANCH_DEV} origin/${BRANCH_DEV})"; then
     git pull origin ${BRANCH_DEV}
     git push origin ${BRANCH_DEV}
 fi
